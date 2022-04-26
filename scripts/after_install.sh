d46b3fe51114622f9f0be2ce0e5b7969712c702a
kboysm <lishal12@naver.com>
2022-04-26 15:46:17 +0900

    test pm2

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,11 +8,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-# kill node
-sudo pkill node
 #!/bin/bash
 REPOSITORY=/home/ec2-user/nest_ec2_actions
 cd $REPOSITORY
 
 npm install
 npm run build
+pm2 kill
+pm2 start 'npm run start:prod'
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+pm2 kill
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/nest_ec2_actions"
 if [ -d "$DIR" ]; then

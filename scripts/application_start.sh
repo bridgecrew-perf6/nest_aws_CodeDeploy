bc5f679ad80840241bd8fee454bee5408e0222d0
kboysm <lishal12@naver.com>
2022-04-26 14:59:40 +0900

    test: npm commend not found

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,11 +7,6 @@ sudo chmod -R 777 /home/ec2-user/nest_ec2_actions
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/nest_ec2_actions
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 #install node modules
 npm run build
 npm run start:prod

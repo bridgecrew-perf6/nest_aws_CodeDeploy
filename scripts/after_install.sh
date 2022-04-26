e7aa5ac3f24cf753b4971c476c8c2809c305ebe6
kboysm <lishal12@naver.com>
2022-04-26 15:12:07 +0900

    test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,19 @@
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 14
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+# kill node
+sudo pkill node
 #!/bin/bash
 REPOSITORY=/home/ec2-user/nest_ec2_actions
 cd $REPOSITORY
 
 sudo npm install
+sudo npm run build
+sudo npm run start:prod
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 14
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-# kill node
-sudo pkill node
-
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/nest_ec2_actions"
 if [ -d "$DIR" ]; then
